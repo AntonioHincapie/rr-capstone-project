a7cb96ed5cc2cb725f1f097e30601f289e9f5a9a
AntonioHincapie <marcohincapiem@gmail.com>
2022-07-12 13:54:59 -0500

    Create Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Details from './Components/Details/Details';
+import Header from './Components/Header/Header';
 import HomePage from './Components/HomePage/HomePage';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:id" element={<Details />} />
