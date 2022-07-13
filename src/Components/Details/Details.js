623e4333014179712bbb02b689be9eb6a8fe943d
AntonioHincapie <marcohincapiem@gmail.com>
2022-07-13 15:09:06 -0500

    Add style to the details page

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,5 +1,6 @@
 import { NavLink, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import closeArrow from '../imgs/back-arrow.png';
 import './Details.css';
 
 export default function Details() {
@@ -10,25 +11,25 @@ export default function Details() {
 
   if (PrintCrypto !== undefined) {
     return (
-      <div>
+      <div className="containerDetails">
         <NavLink to="/">
-          <button type="button">Adios</button>
+          <img src={closeArrow} alt="Back" />
         </NavLink>
         <div
           className="containerCrypto"
           id={id}
         > 
+          <img src={Crypto.img} alt={Crypto.name} />
           <h3>Rank #{Crypto.rank}</h3>
           <h4>{Crypto.name} {Crypto.symbol}</h4>
-          <img src={Crypto.img} alt={Crypto.name} />
           <p className="volumen">
-            Quantity of trading volume represented in USD over the last 24 hours ${Crypto.volumen.toFixed(2)}
+            Quantity of trading volume repr0esented in USD over the last 24 hours: ${Crypto.volumen.toFixed(2)}
           </p>
           <p className="price">
-            Volume-Weighted price based on real-time market data, translated to USD ${Crypto.price.toFixed(2)}
+            Volume-Weighted price based on real-time market data, translated to USD: ${Crypto.price.toFixed(2)}
           </p>
           <p className="change">
-            The direction and value change in the last 24 hours ${Crypto.change.toFixed(2)}
+            The direction and value change in the last 24 hours: ${Crypto.change.toFixed(2)}
           </p>
         </div>
       </div>
